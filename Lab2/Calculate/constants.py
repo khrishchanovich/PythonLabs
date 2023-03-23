5223a975ee993e0a2208e9fbd7556ab5e87ea6a2
khrishchanovich <orannon@gmail.com>
2023-03-23 17:50:27 +0300

    modified main.py and constants.py

--- Lab2/Calculate/constants.py
+++ Lab2/Calculate/constants.py
@@ -1,2 +1,4 @@
 SEPARATORS = r'[.!\?]+'
 NON_DECLARATIVE_SEPARATORS = r'[!\?]+'
+WORD = r'\b[a-zA-Z\d]+\b'
+NUMBER = r'\b\d+\b'
--- Lab2/Calculate/main.py
+++ Lab2/Calculate/main.py
@@ -1,6 +1,6 @@
 import re
-from constants import SEPARATORS, NON_DECLARATIVE_SEPARATORS
-str_ = "Hello! How are you? I'm fine. Thank you... I love you!"
+from constants import SEPARATORS, NON_DECLARATIVE_SEPARATORS, WORD, NUMBER
+str_ = "Hello! How are you? I'm fine. Thank you... I love you! 5gd7 55"
 
 len_all = len(re.findall(SEPARATORS, str_))
 
@@ -10,4 +10,23 @@ len_non_declarative = len(re.findall(NON_DECLARATIVE_SEPARATORS, str_))
 
 print(len_non_declarative)
 
+print(str_)
+numbers = re.findall(NUMBER, str_)
+words_re = re.findall(WORD, str_)
+words = []
+for i in words_re:
+    if i not in numbers:
+        words.append(i)
+sentence_len_in_characters = 0
+for i in words:
+    sentence_len_in_characters += len(i)
+print(sentence_len_in_characters)
+average_length_sentences = sentence_len_in_characters / len_all
+print(average_length_sentences)
+
+words_len = len(words)
+print(words_len)
+
+average_length_words = sentence_len_in_characters / words_len
+print(average_length_words)
 
