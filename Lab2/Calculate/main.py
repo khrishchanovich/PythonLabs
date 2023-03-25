a91ba64b981d5bc5f1f8f1d7f5b00111b54a2473
khrishchanovich <orannon@gmail.com>
2023-03-25 22:08:11 +0300

    modified main.py

--- Lab2/Calculate/main.py
+++ Lab2/Calculate/main.py
@@ -1,7 +1,8 @@
 from function import amount_of_sentences, \
     amount_of_non_declarative_sentences, \
     average_length_of_the_sentences, \
-    average_length_of_the_world
+    average_length_of_the_world, \
+    top_k_repeated_n_grams
 
 str_ = input('Enter text: ')
 
@@ -9,9 +10,10 @@ script1 = amount_of_sentences(str_)
 script2 = amount_of_non_declarative_sentences(str_)
 script3 = average_length_of_the_sentences(str_)
 script4 = average_length_of_the_world(str_)
-# script 5
+script5 = top_k_repeated_n_grams(str_)
 
 print('Amount of sentences: ', script1,
       '\nAmount of non-declarative sentences: ', script2,
       '\nAverage length of the sentences in characters: ', script3,
-      '\nAverage length of the world in the text in characters: ', script4)
+      '\nAverage length of the world in the text in characters: ', script4,
+      '\nTop-K repeated N-grams in the text (K=10, N=4):', script5)
