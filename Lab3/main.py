c95146cc107429c8009188463dd1ef0690062466
khrishchanovich <orannon@gmail.com>
2023-04-03 23:07:56 +0300

    add main.py

---
+++ Lab3/main.py
@@ -0,0 +1,12 @@
+"""Classes:
+            Pickle
+            Unpickle
+
+    Functions:
+            dump(object, file)
+            dumps(object) -> string
+            load(file) -> object
+            loads(bytes) -> object"""
+
+from _pickle import *
+
