959b06dda2f3731f5843398b4e98a69cbd192473
khrishchanovich <orannon@gmail.com>
2023-05-09 15:44:41 +0300

    modified factory.py

--- Lab3/Factory/factory.py
+++ Lab3/Factory/factory.py
@@ -1,5 +1,6 @@
 from Lab3.Factory.constants import JSON, XML
 from Lab3.Parser.json.json_parser import Json
+from Lab3.Parser.xml.xml_parser import Xml
 
 
 class Factory(object):
@@ -8,5 +9,7 @@ class Factory(object):
     def parser(type_: str):
         if type_.__eq__(JSON):
             return Json
+        elif type_.__eq__(XML):
+            return Xml
         else:
             return Json
