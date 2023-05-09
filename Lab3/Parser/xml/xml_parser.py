e3788132d7deb20564876effc13423ee3f182846
khrishchanovich <orannon@gmail.com>
2023-05-09 19:20:14 +0300

    modified xml_parser.py

--- Lab3/Parser/xml/xml_parser.py
+++ Lab3/Parser/xml/xml_parser.py
@@ -1,20 +1,18 @@
 from Lab3.Parser.parser import Parser
 from Lab3.Parser.xml.xml import from_string_objects, to_string_objects, from_dict, to_dict
-
+from json import dump
 
 class Xml(Parser):
     def dump(self, obj, file):
         with open(file, 'w+') as f:
-            f.write(str(self.dumps(obj)))
+            f.write(self.dumps(obj))
 
     def dumps(self, obj):
-        obj_ = to_dict(obj)
-        return from_dict(to_string_objects(obj_))
+        return to_dict(to_string_objects(obj))
 
     def load(self, file):
         with open(file, 'r') as f:
-            return self.loads(eval(f.read()))
+            return self.loads(file)
 
     def loads(self, string):
-        obj_ = from_dict(to_dict(string))
-        return from_string_objects(obj_)
+        return from_dict(from_string_objects(string))
